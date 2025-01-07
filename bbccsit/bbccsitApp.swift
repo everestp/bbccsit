fe1882d18b6b32d5b51c743758354f84e123c10b
everestp <145990676+everestp@users.noreply.github.com>
2025-01-07 11:02:08 +0545

    Initial Commit

---
+++ bbccsit/bbccsitApp.swift
@@ -0,0 +1,17 @@
+//
+//  bbccsitApp.swift
+//  bbccsit
+//
+//  Created by Everest Paudel on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct bbccsitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
